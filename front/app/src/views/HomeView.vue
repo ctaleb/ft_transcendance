d96c5072e44a096e8d263f004a6ad65d0406f8f5
Loic Fourmaux <lfourmaux@birdyx.fr>
2022-07-28 16:17:12 +0200

    Tests on responsive bootstrap

--- front/app/src/views/HomeView.vue
+++ front/app/src/views/HomeView.vue
@@ -6,6 +6,26 @@
   <p class="text-success" @click="increment()">{{ number }}</p>
   <input type="text" name="hey" id="ho" v-model="text" />
   <h2 class="text-warning">{{ text }}</h2>
+  <div class="row mt-3">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</div>
 </div>
 </template>
 
