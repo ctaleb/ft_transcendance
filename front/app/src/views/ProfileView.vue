e49b7f5edeafcba1e51c2a6391323f972dda4a14
Loic Fourmaux <lfourmaux@birdyx.fr>
2022-08-23 10:27:26 +0200

    Profile picture is finally displayed

--- front/app/src/views/ProfileView.vue
+++ front/app/src/views/ProfileView.vue
@@ -3,6 +3,7 @@
 		<p>Hello {{ user.nickname }} !</p>
 		<p>Created at {{ user.createdAt }}</p>
 		<p>Updated at {{ user.updatedAt }}</p>
+		<img :src="image" alt="" style="max-width: 450px; max-height: 300px">
 	</div>
 	<p v-else>User = null</p>
 </template>
@@ -15,6 +16,8 @@ export default defineComponent({
 	data() {
 		return {
 			user: JSON.parse(localStorage.getItem("user") || '{}'),
+			avatar: '{}',
+			image: "",
 		};
 	},
 	
@@ -29,6 +32,31 @@ export default defineComponent({
     .then((data) => {
 		if (data.message)
 			this.$router.push('/');
+		else
+		{
+			fetch("http://localhost:3000/api/user/" + this.user.id, {
+				method: "GET",
+				headers: {
+					"Authorization": "Bearer " + localStorage.getItem("token"),
+				},
+			})
+			.then(res => res.json())
+			.then((data) => {
+				this.avatar = data.path;
+				console.log(this.avatar);
+				fetch("http://localhost:3000/api/user/profile-picture/" + this.avatar, {
+					method: "GET",
+					headers: {
+						"Authorization": "Bearer " + localStorage.getItem("token"),
+					},
+				})
+				.then(res => res.blob())
+				.then((data) => {
+					console.log(data);
+					this.image = URL.createObjectURL(data);
+				})
+			})
+		}
 	})
     .catch(err => console.log(err.message));
   }
