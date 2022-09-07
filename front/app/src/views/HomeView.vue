909f0e93260af0c0e8ae2c21b6ccd0d193b33e8e
Loic Fourmaux <lfourmaux@birdyx.fr>
2022-09-07 09:02:23 +0200

    Just delete async/await passing second fetch in then

--- front/app/src/views/HomeView.vue
+++ front/app/src/views/HomeView.vue
@@ -58,9 +58,9 @@ methods: {
 	})
 	},
 
-	async oauth() {
+	oauth() {
 
-	await fetch("https://api.intra.42.fr/oauth/token", {
+	fetch("https://api.intra.42.fr/oauth/token", {
 		body: "grant_type=client_credentials&client_id=1a90768d9956eae0b0360b4588273a1d4a25143a9c8cfc6a0330dac17b9684db&client_secret=fc3b24dee46ae52d1c335e8ef794d2de3dcd530b97fa22aeb19267de032a9f49",
 		headers: {
 			"Content-Type": "application/x-www-form-urlencoded"
@@ -68,15 +68,17 @@ methods: {
 		method: "POST"
 	})
 	.then((res) => res.json())
-	.then((value) => { this.token = value; console.log("TOKEN : " + this.token.access_token); })
-	.catch((err) => {console.log(err); })
-	console.log("TOKEN2 : " + this.token.access_token);
-	fetch("https://api.intra.42.fr/v2/cursus", {
-		headers: {
-			"Authorization": "Bearer " + this.token.access_token,
-		},
+	.then((value) => { 
+		this.token = value; 
+		fetch("https://api.intra.42.fr/v2/cursus", {
+			headers: {
+				"Authorization": "Bearer " + this.token.access_token,
+			},
+		})
+		.then((res) => res.json())
+		.then((value) => {console.log(value);})
 	})
-	.then((value) => {console.log(value.json())});
+	.catch((err) => {console.log(err); })
 
 	},
 },
