e0c6e6125dbd03fc268b2ec97076ced40fc916a3
Loic Fourmaux <lfourmaux@birdyx.fr>
2022-09-08 14:34:50 +0200

    TOKEN IS HERE BRO

--- back/app/src/oauth/oauth.service.ts
+++ back/app/src/oauth/oauth.service.ts
@@ -6,18 +6,12 @@ import { UpdateOauthDto } from './dto/update-oauth.dto';
 @Injectable()
 export class OauthService {
   connect(code: string) {
-    
+    console.log(code);
     fetch("https://api.intra.42.fr/oauth/token", {
       headers: {
         "Content-Type": "multipart/form-data",
       },
-      body: JSON.stringify({
-        grant_type: "authorization_code",
-        client_id: "1a90768d9956eae0b0360b4588273a1d4a25143a9c8cfc6a0330dac17b9684db",
-        client_secret: "fc3b24dee46ae52d1c335e8ef794d2de3dcd530b97fa22aeb19267de032a9f49",
-        code: code,
-        redirect_uri: "http%3A%2F%2Flocalhost%3A3000%2F",
-      }),
+      body: 'grant_type=authorization_code&client_id=1a90768d9956eae0b0360b4588273a1d4a25143a9c8cfc6a0330dac17b9684db&client_secret=fc3b24dee46ae52d1c335e8ef794d2de3dcd530b97fa22aeb19267de032a9f49&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&code=' + code,
       method: "POST"
     })
     .then((val) => val.json())
