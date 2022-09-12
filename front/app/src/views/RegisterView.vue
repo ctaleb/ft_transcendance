1275e252ccfa526086707657d0448add8dbcfe73
Nicolas Catriens <nicolas.catriens@gmail.com>
2022-09-12 14:51:34 +0200

    Compiling, but the v$ object seems uninitialized

--- front/app/src/views/RegisterView.vue
+++ front/app/src/views/RegisterView.vue
@@ -1,144 +1,145 @@
 <template>
-	<form>
-		<label for="nickname">Nickname:</label><br />
-		<input
-			type="text"
-			v-model="state.nickname"
-			id="nick"
-			name="nickname"
-			required
-		/>
-        <br /><br />
-		<label for="password">Password:</label><br />
-		<input
-			type="password"
-			v-model="state.password.firstTry"
-			id="password"
-			name="password"
-			required
-		/>
-        <br /><br />
-        <label for="confirm password">Confirm password:</label><br />
-        <input
-            type="password"
-			v-model="state.password.confirmation"
-            placeholder="Confirm password"
-            autocomplete="off"
-        /><br /><br />
-		<label for="phone">Phone number:</label><br />
-		<input
-			type="tel"
-			v-model="state.phone"
-			id="phone"
-			name="phone"
-			placeholder="0611111111"
-			pattern="[0-9]{10}"
-			required
-		/><br /><br />
-		<label for="avatar">Choose a profile picture:</label><br />
-		<input
-			type="file"
-			id="avatar"
-			name="avatar"
-			accept="image/*"
-			@change="updateAvatar"
-		/><br /><br />
-		<input type="submit" value="Submit" @click.stop.prevent="submitForm()" />
+  <form>
+    <label for="nickname">Nickname:</label><br />
+    <input
+           type="text"
+           v-model="state.nickname"
+           id="nick"
+           name="nickname"
+           required
+           />
+    <br /><br />
+    <label for="password">Password:</label><br />
+    <input
+           type="password"
+           v-model="state.password.firstTry"
+           id="password"
+           name="password"
+           required
+           />
+    <br /><br />
+    <label for="confirm password">Confirm password:</label><br />
+    <input
+           type="password"
+           v-model="state.password.confirmation"
+           placeholder="Confirm password"
+           autocomplete="off"
+           /><br /><br />
+           <label for="phone">Phone number:</label><br />
+           <input
+                  type="tel"
+                  v-model="state.phone"
+                  id="phone"
+                  name="phone"
+                  placeholder="0611111111"
+                  pattern="[0-9]{10}"
+                  required
+                  /><br /><br />
+                  <label for="avatar">Choose a profile picture:</label><br />
+                  <input
+                         type="file"
+                         id="avatar"
+                         name="avatar"
+                         accept="image/*"
+                         @change="updateAvatar"
+                         /><br /><br />
+                         <input type="submit" value="Submit" @click.stop.prevent="submitForm()" />
 
-	</form>
+  </form>
 </template>
 
 <script lang="ts">
 
 import { defineComponent, reactive, computed } from "vue";
+import { useRouter } from 'vue-router';
 import useVuelidate from '@vuelidate/core';
 import { required, minLength, maxLength, sameAs } from '@vuelidate/validators';
 
+
+const o = {
+	a: 1,
+	b: 1,
+	c: 1,
+	d: 1,
+};
+
+
 export default defineComponent({
-	name: "RegisterView", //celle la j'en suis sur
+  name: "RegisterView",
 
-	setup() {
+  setup() {
 
-		const state = reactive({
-			nickname: "",
-			password: {
-				firstTry: "",
-				confirmation: "",
-			},
-			phone: "",
-		})
+    const state = reactive({
+      nickname: "",
+      password: {
+        firstTry: "",
+        confirmation: "",
+      },
+      phone: "",
+      avatar: File.prototype,
+    })
 
-		const rules = computed(() => ({
-			nickname: { required },
-			password: {
-				firstTry: { required },
-				confirmation: { required, sameAs: sameAs(state.password.firstTry) },
-			},
-			phone: { required }
-        }))
+    const router = useRouter()
 
-		const v$ = useVuelidate(rules, state)
+    function updateAvatar(event: Event) {
+      state.avatar = (event.target as HTMLInputElement).files?.[0]!;
+    }
 
-		return { state, v$ }
-			
-	},
+    async function createPost() {
+      let formData = new FormData();
 
-	methods: {
-		updateAvatar(event: Event) {
-			this.avatar = (event.target as HTMLInputElement).files?.[0]!;
-		},
+      //this.submitted = true;
 
+      formData.append("nickname", state.nickname);
+      formData.append("phone", state.phone);
+      formData.append("password", state.password.firstTry);
+      if (state.avatar != File.prototype) {
+        formData.append("avatar", state.avatar, state.avatar.name);
+      }
 
-		async createPost() {
-			let formData = new FormData();
+      fetch("http://localhost:3000/api/authentication/registration", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          router.push("/");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
 
-            this.submitted = true;
+    const rules = computed(() => ({
+      nickname: { required },
+      password: {
+        firstTry: { required },
+        confirmation: { required, sameAs: sameAs(state.password.firstTry) },
+      },
+      phone: { required },
+    }))
 
-			formData.append("nickname", this.nickname);
-			formData.append("phone", this.phone);
-			formData.append("password", this.password.firstTry);
-			if (this.avatar != File.prototype) {
-				formData.append("avatar", this.avatar, this.avatar.name);
-			}
+    const v$ = useVuelidate(rules, state);
 
-			fetch("http://localhost:3000/api/authentication/registration", {
-				method: "POST",
-				body: formData,
-			})
-				.then((response) => response.json())
-				.then((data) => {
-					this.$router.push("/");
-				})
-				.catch((err) => {
-				//	this.$router.push("/");
-					console.log(err);
-				});
-		},
+    function submitForm() {
+      console.log(v$);
+      console.log(state);
+      console.log(rules);
+    }
 
-		submitForm() {
-			console.log(this.v$);
-			this.v$.$validate();
-			if (this.v$.$error) {
-				console.log("error detected in form");
-				console.log("password: " + this.password.firstTry);
-				console.log("password.confirm: " + this.password.confirmation);
-			} else {
-				this.createPost();
-			}
-
-		},
-	},
+    return { state, v$, updateAvatar, createPost, submitForm };
+  }
 });
+
 </script>
+
 <style scoped>
 p {
-	user-select: none;
+  user-select: none;
 }
-</style>
 
-<style>
 .greeting {
-	color: red;
-	font-weight: bold;
+  color: red;
+  font-weight: bold;
 }
 </style>
