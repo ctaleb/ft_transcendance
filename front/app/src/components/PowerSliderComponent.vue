bd36b2124d0cdcfc822524d7f653f631dc8fec95
Nicolas Catriens <nicolas.catriens@gmail.com>
2022-09-26 11:15:36 +0200

    Slider works with image and icon. TODO: position on the page

--- front/app/src/components/PowerSliderComponent.vue
+++ front/app/src/components/PowerSliderComponent.vue
@@ -1,66 +1,74 @@
 <template>
-    <swiper
-      :slides-per-view="4"
-      :space-between="30"
-      @swiper="onSwiper"
-      @slideChange="onSlideChange"
-      class="default-slider"
-    >
-      <swiper-slide v-for="n in 7" :key="n"> {{ n }} </swiper-slide>
-    </swiper>
-  </template>
-  
-  <style scoped>
-  .default-slider .swiper-slide {
-    display: flex;
-    height: 300px !important;
-    justify-content: center;
-    align-items: center;
-    color: #000;
-    font-size: 24px;
-    font-weight: 700;
+  <div class="items">
+		<div class="item active">
+			<img src="https://static.onzemondial.com/photos/283770/carre-psg--la-toile-encourage-mbapp%C3%A9-pour-le-choc-face-au-rb-leipzig.jpg">
+		</div>
+		<div class=" item next">
+			<img src="https://static.onzemondial.com/photos/283770/carre-psg--la-toile-encourage-mbapp%C3%A9-pour-le-choc-face-au-rb-leipzig.jpg">
+		</div>
+		<div class="item">
+			<img src="https://static.onzemondial.com/photos/283770/carre-psg--la-toile-encourage-mbapp%C3%A9-pour-le-choc-face-au-rb-leipzig.jpg">
+		</div>
+		<div class="item">
+			<img src="https://static.onzemondial.com/photos/283770/carre-psg--la-toile-encourage-mbapp%C3%A9-pour-le-choc-face-au-rb-leipzig.jpg">
+		</div>
+		<div class="item prev">
+			<img src="https://static.onzemondial.com/photos/283770/carre-psg--la-toile-encourage-mbapp%C3%A9-pour-le-choc-face-au-rb-leipzig.jpg">
+		</div>
+		<div class="button-container">
+			<div class="button"><font-awesome-icon  icon="fa-solid fa-toilet-paper"/></div>
+			<div class="button"><font-awesome-icon  icon="fa-solid fa-toilet-paper"/></div>
+		</div>
+	</div>
+</template>
+<script lang="ts" >
+
+  import "./style.css";
+
+export default{
+  mounted(){
+    // I will be creating a different pen with touch support but right // now I have no time for it due to school
+
+const slider = document.querySelector(".items");
+const slides = document.querySelectorAll(".item");
+const button = document.querySelectorAll(".button");
+
+let current = 0;
+let prev = 4;
+let next = 1;
+
+for (let i = 0; i < button.length; i++) {
+  button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
+}
+
+const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
+
+const gotoNext = () => current < 4 ? gotoNum(current + 1) : gotoNum(0);
+
+const gotoNum = (number: number) => {
+  current = number;
+  prev = current - 1;
+  next = current + 1;
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+    slides[i].classList.remove("prev");
+    slides[i].classList.remove("next");
   }
-  .swiper-slide:nth-child(1n) {
-    background-color: palevioletred;
+
+  if (next == 5) {
+    next = 0;
   }
-  .swiper-slide:nth-child(2n) {
-    background-color: skyblue;
+
+  if (prev == -1) {
+    prev = 4;
   }
-  .swiper-slide:nth-child(3n) {
-    background-color: peru;
+
+  slides[current].classList.add("active");
+  slides[prev].classList.add("prev");
+  slides[next].classList.add("next");
   }
-  .swiper-slide:nth-child(4n) {
-    background-color: cadetblue;
-  }
-  .swiper-slide:nth-child(5n) {
-    background-color: plum;
-  }
-  .swiper-slide:nth-child(6n) {
-    background-color: goldenrod;
-  }
-  .swiper-slide:nth-child(7n) {
-    background-color: palegreen;
-  }
-  </style>
-  
-  <script>
-  // Import Swiper Vue.js components
-  //import { SwiperSlide } from "swiper/vue/swiper-slide";
-  import { Swiper, SwiperSlide } from "swiper/vue";
-  // Import Swiper styles
-  import "swiper/swiper.min.css";
-  export default {
-    components: {
-      Swiper,
-      SwiperSlide
-    },
-    methods: {
-      onSwiper: function () {
-        console.log("onSwiper called");
-      },
-      onSlideChange: function () {
-        console.log("Slide change");
-      },
-    },
-  };
-  </script>
+}
+}
+</script>
+
--- front/app/src/main.ts
+++ front/app/src/main.ts
@@ -2,5 +2,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faToiletPaper } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faToiletPaper)
+
+createApp(App)
+.use(router)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
 
