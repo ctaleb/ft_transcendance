f35f39bfbb521e14c6199fab9e235e05047d054c
MauriceNDS <ds.arsen25@gmail.com>
2022-11-19 00:17:23 +0100

    Added helper functions

--- front/app/src/functions/funcs.ts
+++ front/app/src/functions/funcs.ts
@@ -29,23 +29,29 @@ export function getUserById(id: number): Promise<any> {
     .catch((err) => console.log(err.message));
 }
 
-export function getUserAvatar(avatar: String): Promise<void | Blob> {
-  return fetch(
-    "http://" +
-      window.location.hostname +
-      ":3000/api/user/profile-picture/" +
-      avatar,
-    {
-      method: "GET",
-      headers: {
-        Authorization: "Bearer " + localStorage.getItem("token"),
-      },
-    }
-  )
-    .then((res) => {
-      return res.blob();
-    })
-    .catch((err) => console.log(err.message));
+export function getUserAvatar(avatar: String): Promise<Blob> {
+  return fetchBlobDatas(`api/user/profile-picture/${avatar}`, "GET");
+}
+
+async function fetchDatas(
+  path: string,
+  method: "GET" | "PUT" | "POST" | "DELETE",
+  body?: object
+): Promise<any> {
+  return fetch(`http://${window.location.hostname}:3000/${path}`, {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("token"),
+    },
+    body: JSON.stringify(body),
+  });
+}
+
+async function getErrorMessage(response: Response): Promise<string> {
+  return await response.json()
+    .then((d: { message: string }) => d.message)
+    .catch((e: string) => e);
 }
 
 export async function fetchJSONDatas(
@@ -62,28 +68,40 @@ export async function fetchJSONDatas(
   method: "GET" | "PUT" | "POST" | "DELETE",
   body?: object
 ): Promise<any> {
-  return fetch(`http://${window.location.hostname}:3000/${path}`, {
-    method: method,
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + localStorage.getItem("token"),
-    },
-    body: JSON.stringify(body),
-  })
+  return fetchDatas(path, method, body)
     .then((res: Response) => {
       if (!res.ok) return Promise.reject(res);
       return res.json();
     })
-    .catch((err: Response) => {
-      let message: string;
-      err
-        .json()
-        .then((d: { message: string }) => {
-          message = d.message;
-        })
-        .catch((e: any) => (message = e))
-        .finally(() => {
-          console.log(message);
-        });
+    .catch(async (err: Response) => {
+      let message = await getErrorMessage(err);
+      console.log(message);
+      return Promise.reject(message);
+    });
+}
+
+export async function fetchBlobDatas(
+  path: string,
+  method: "GET" | "PUT" | "POST" | "DELETE"
+): Promise<any>;
+export async function fetchBlobDatas(
+  path: string,
+  method: "GET" | "PUT" | "POST" | "DELETE",
+  body: object
+): Promise<any>;
+export async function fetchBlobDatas(
+  path: string,
+  method: "GET" | "PUT" | "POST" | "DELETE",
+  body?: object
+): Promise<Blob> {
+  return fetchDatas(path, method, body)
+    .then((res: Response) => {
+      if (!res.ok) return Promise.reject(res);
+      return res.blob();
+    })
+    .catch(async (err: Response) => {
+      let message = await getErrorMessage(err);
+      console.log(message);
+      return Promise.reject(message);
     });
 }
