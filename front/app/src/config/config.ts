b753b368828cbf617dd6dc47a95e8a9eb121c8cf
ctaleb <76259856+ctaleb@users.noreply.github.com>
2022-12-10 13:42:46 +0100

    Socket connection fix & removal of some old chat logic (#113)
    * added support for multiclienting by effectively disabling it
    
    * fixed some socket connection issues; removed old unused chat logic dating from first chat version
    
    Co-authored-by: Nicolas Catriens <nicolas.catriens@gmail.com>
    Co-authored-by: Arsène <31856200+MauriceNDS@users.noreply.github.com>

--- front/app/src/config/config.ts
+++
@@ -1,9 +0,0 @@
-import { io, Socket } from "socket.io-client";
-
-export default {
-  socket: io("http://" + window.location.hostname + ":3500", {
-    transports: ["websocket"],
-    // path: "/api/socket.io/",
-    autoConnect: false,
-  }),
-};
